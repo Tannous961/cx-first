d51d1e68b2657115202582b8b9e8f044df324e15
Rémy Bauduin <rbauduin@atecna.fr>
2025-07-24 09:38:38 +0200

    Add footer info from Strapi

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
-
-import { API_URL } from "@/lib/config";
+import { API_URL } from '@/lib/config';
 
 export type ApiResult<T> = {
-    data: T;
+  data: T;
   meta: {
     pagination?: {
       page: number;
@@ -11,21 +10,32 @@ export type ApiResult<T> = {
       total: number;
     };
   };
-}
+};
 
-export const getApiData = async <T>(endpoint: string, lang: string): Promise<ApiResult<T> | null> => {
+export const getApiData = async <T>(
+  endpoint: string,
+  lang: string,
+  populate?: string
+): Promise<ApiResult<T> | null> => {
   try {
-    const response = await fetch(`${API_URL}/api/${endpoint}?locale=${lang}`, { next: { revalidate: 3600 } });
+    const params = new URLSearchParams();
+    params.append('lang', lang);
+    if (populate) {
+      params.append('populate', populate);
+    }
+    const response = await fetch(
+      `${API_URL}/api/${endpoint}?${params.toString()}`,
+      { next: { revalidate: 3600 } }
+    );
 
     if (!response.ok) {
       throw new Error(`HTTP error status: ${response.status}`);
     }
 
     const data = await response.json();
-
     return data as ApiResult<T>;
   } catch (error) {
     console.error(error);
     return null;
   }
-}
+};
--- src/services/global.ts
+++ src/services/global.ts
@@ -2,22 +2,22 @@
 import { getApiData } from "./api";
 
 export type FooterData = {
-    id: number;
-    documentId: string;
-    Newsletter: string;
-    Product: string;
-    Legal: string;
-    Social: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    locale: string;
-    newsletterTitle: string | null;
-    productTitle: string | null;
-    legalTitle: string | null;
-    socialTitle: string | null;
+  id: number;
+  documentId: string;
+  Newsletter: string;
+  Product: string;
+  Legal: string;
+  Social: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  locale: string;
+  newsletterTitle: string | null;
+  productTitle: string | null;
+  legalTitle: string | null;
+  socialTitle: string | null;
 };
 
 export const getFooterData = async (lang: string) => {
   return await getApiData<FooterData>('footer', lang);
-}
+};
